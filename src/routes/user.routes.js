07f57d1798d6989c189b87c23dd5551b0bc6525c
M. Hassan <hassanasif419@gmail.com>
2024-09-12 13:08:39 +0500

    Fixed an typo in path of user.routes.js

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,7 +3,7 @@ import { registerUser } from "../controllers/user.controller.js";
 
 const router = Router()
 
-router.route("register").post(registerUser)
+router.route("/register").post(registerUser)
 
 export default router
 
