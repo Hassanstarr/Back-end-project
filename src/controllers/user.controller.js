a300c55fe371d869b3d48a22577575707330bb75
M. Hassan <hassanasif419@gmail.com>
2024-09-18 12:16:37 +0500

    Add user account management endpoint

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -235,9 +235,133 @@ const refreshAccessToken = asyncHandler( async(req,res) => {
 } )
 
 
+const changeCurrentPassword = asyncHandler( async(req, res) => {
+    const {oldPassword, newPassword} = req.body
+
+    const user = await User.findById(req.user?._id)
+
+    const isPasswordCorrect = new user.isPasswordCorrect(oldPassword)
+
+    if(!isPasswordCorrect) {
+        throw new ApiError(400, "Invalid old password")
+    }
+
+    user.password = newPassword
+    await user.save({ validateBeforeSave: false })
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Password change successfully"))
+    
+} )
+
+
+const getCurrentUser = asyncHandler( async(req, res) => {
+    return res
+    .status(200
+    .json(new ApiResponse(200, req.user, "current user fethced successfully"))
+    )
+} )
+
+
+const updateAccountDetails = asyncHandler( async(req, res) => {
+    const {fullName, email} = req.body
+    
+    if(!fullName || !email) {
+        throw new ApiError(400, "All field required")
+    }
+
+    const user = User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set: {
+                fullName,
+                email: email    // both typing method is correct
+            }
+        },
+        { new: true  } // if new is true than if retuen the data after it it save is the database
+    ).select("-password")
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, user, "Account detail update successfully"))
+    
+} )
+
+
+const updateUserAvatar = asyncHandler( async(req, res) => {
+    const avatarLocalPath = res.file?.path
+
+    if(!avatarLocalPath) {
+        throw new ApiError(400, "Avatar file missing")
+    }
+
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
+
+    if(!avatar.url){
+        throw new ApiError(400, "Error while uploading avatar")
+    }
+
+    const user = await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set: {
+                avatar: avatar.url
+            }
+        },
+        {new: true}
+    ).select("-password")
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, user, "Avatar update successfully"))
+    
+    
+} )
+
+
+const updateUserCoverImage = asyncHandler( async(req, res) => {
+    const coverImageLocalPath = res.file?.path
+
+    if(!coverImageLocalPath) {
+        throw new ApiError(400, "Cover Image file missing")
+    }
+
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+    if(!coverImage.url){
+        throw new ApiError(400, "Error while uploading cover image")
+    }
+
+    const user = await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set: {
+                coverImage: coverImage.url
+            }
+        },
+        {new: true}
+    ).select("-password")
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, user, "Cover Image update successfully"))
+    
+    
+} )
+
+
+
+
+
 export {
     registerUser,
     loginUser,
     logoutUser,
-    refreshAccessToken
+    refreshAccessToken,
+    changeCurrentPassword,
+    getCurrentUser,
+    updateAccountDetails,
+    updateUserAvatar,
+    updateUserCoverImage
 }
